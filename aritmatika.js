a3180b16c544e2448c045172f14ab9f4c4bba91a
M. Irfan Baari <mirfanbaari40@gmail.com>
2025-03-16 01:19:47 +0700

    Add files via upload

---
+++ aritmatika.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Operasi Aritmatika</title>
+</head>
+<body bgcolor="blue">
+		<h2>Operasi Aritmatika</h2>
+			<script language="javascript">
+				document.write ("10 + 4" + (1=+4) + "<br>");
+				document.write ("20 -3 = " + (20-3) + "<br>");
+				document.write ("5 * 3 = " + (5*3) + "<br>");
+				document.write ("40 / 3 = " + (40/3) + "<br>");
+				document.write ("20 % 3 = " + (20%3) + "<br>");
+		</script>
+</body>
+</html>
